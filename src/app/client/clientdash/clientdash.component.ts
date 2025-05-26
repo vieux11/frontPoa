7acae340c2a9ea544676f5bef19dfd41ef70c8cf
Berdin Matondo <berdinmatondo7@gmail.com>
2025-05-26 13:37:21 +0100

    dashboard client

--- src/app/client/clientdash/clientdash.component.ts
+++ src/app/client/clientdash/clientdash.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { ReservationService } from '../../core/services/reservation.service';
+import { Reservation } from '../../models/reservation';
+import { ReservationComponent } from '../reservation/reservation.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-clientdash',
-  imports: [],
+  imports: [ReservationComponent, NgFor, NgIf],
   templateUrl: './clientdash.component.html',
   styleUrl: './clientdash.component.css'
 })
 export class ClientdashComponent {
+  reservations: Reservation[] = [];
+  loading = true;
 
+  constructor(private reservationService: ReservationService) {}
+
+  ngOnInit(): void {
+    this.reservationService.getMyReservations().subscribe({
+      next: (data) => {
+        this.reservations = data;
+        console.log('Réservations chargées', this.reservations);
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Erreur chargement des réservations', err);
+        this.loading = false;
+      },
+    });
+  }
+
+  handleDownload(reservationId: number) {
+    console.log('Téléchargement du billet ID', reservationId);
+    // TODO: implémentation du téléchargement
+  }
 }
