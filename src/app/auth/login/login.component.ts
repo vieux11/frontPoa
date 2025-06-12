67a984f787589a589299cc21c52cee0a7dbd3ec8
Berdin Matondo <berdinmatondo7@gmail.com>
2025-06-12 18:07:03 +0100

    validator login and toast

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,43 +1,55 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 import { Router, RouterLink } from '@angular/router';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, NgIf, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   loginForm: FormGroup;
-  Submitted = false;
+  submitted = false;
+  showSuccessToast = false;
+  apiError = '';
 
   constructor(private authService: AuthService, private router: Router, private fb:FormBuilder) {
     this.loginForm = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', [Validators.email, Validators.required]]
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
 
   handleSubmit() {
-    this.authService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.invalid) return;
+
+    this.authService.login(
+      this.loginForm.value.email,
+      this.loginForm.value.password
+    ).subscribe({
       next: (res) => {
+        this.showSuccessToast = true;
         this.authService.loginSuccess(res);
-        const role = this.authService.getRole();
-        if (role === 'admin') this.router.navigate(['/admin']);
-        else this.router.navigate(['/client']);
+        
+        setTimeout(() => {
+          const role = this.authService.getRole();
+          role === 'admin' 
+            ? this.router.navigate(['/admin']) 
+            : this.router.navigate(['/client']);
+        }, 1500);
       },
       error: (err) => {
-        console.error('Erreur de connexion :', err);
-        alert("Email ou mot de passe incorrect !");
-        this.Submitted = true;
+        this.apiError = err.error?.message || 'Email ou mot de passe incorrect';
+        this.submitted = true;
       }
     });
   }
 
-close() {
-  this.Submitted = false
-}
+  closeError() {
+    this.submitted = false;
+  }
 }
