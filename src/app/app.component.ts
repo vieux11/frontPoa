4ba91da798222696f1310471d34d5a478018f61d
Berdin Matondo <berdinmatondo7@gmail.com>
2025-08-29 17:43:15 +0100

    feat:nouveau titre et injection toast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { ToastComponent } from './shared/toast/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ToastComponent],
   templateUrl: './app.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontPoa';
+  title = 'Poaevent';
 }
