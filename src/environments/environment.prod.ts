6fd6f601ad3311e05beec80e8cd731f9e83fe330
Berdin Matondo <berdinmatondo7@gmail.com>
2025-08-29 15:11:03 +0100

    refactor:suppression env.local

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-// environments/environment.ts
-export const environment = {
-  production: true,
-  apiUrl: 'https://poa-app-adonis-app-c7911a78.onrender.com', // Adonis
-  socketServerUrl: 'https://poa-app-socket-server-c7911a78.onrender.com' // Serveur Socket.io
-};
-
