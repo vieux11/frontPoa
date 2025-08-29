1202bcd130559aee3e5a25ba80bdb1bc43d983d5
Berdin Matondo <berdinmatondo7@gmail.com>
2025-08-29 15:03:25 +0100

    variables de developpement en local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 // environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'https://poa-app-adonis-app-c7911a78.onrender.com', // Adonis
-  socketServerUrl: 'https://poa-app-socket-server-c7911a78.onrender.com' // Serveur Socket.io
+  apiUrl: 'http://127.0.0.1:3333', // Adonis
+  socketServerUrl: 'http://127.0.0.1:5050' // Serveur Socket.io
 };
-
