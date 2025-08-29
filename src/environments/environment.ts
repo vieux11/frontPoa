5cdcc54b1ee01f3cf02113e30789c63a0f0bb35a
Berdin Matondo <berdinmatondo7@gmail.com>
2025-08-29 15:11:21 +0100

    one env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 // environments/environment.ts
 export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:3333', // Adonis
-  socketServerUrl: 'http://127.0.0.1:5050' // Serveur Socket.io
+  production: true,
+  apiUrl: 'https://poa-app-adonis-app-c7911a78.onrender.com', // Adonis
+  socketServerUrl: 'https://poa-app-socket-server-c7911a78.onrender.com' // Serveur Socket.io
 };
+
